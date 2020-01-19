a1f95e053d9581fb8bae43cfc1d95e09b14f44b6
NkanyisoSigwaza <38808434+NkanyisoSigwaza@users.noreply.github.com>
2020-01-19 23:50:19 +0200

    Create Property.js

---
+++ models/Property.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create property Schema
+const PropertySchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name field is required'],
+  },
+  address: {
+    type: String,
+    required: [true, 'Adress field is required'],
+  },
+  price: {
+    type: String,
+    required: [true, 'Price field is required'],
+  },
+  agent: {
+    type: String,
+    required: [true, 'Agent field is required'],
+  },
+  //   img: {
+  //     data: Buffer,
+  //     contentType: String, //buffer allows you to store images as data in the form of arrays
+  //   },
+
+  // picture
+  // area square meters
+});
+
+const Property = mongoose.model('property', PropertySchema); // our model to be structured on ninja Schema
+
+module.exports = Property;
